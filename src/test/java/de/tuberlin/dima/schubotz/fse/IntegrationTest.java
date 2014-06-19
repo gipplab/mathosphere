474f46e3b01b37a4ddc16b2edf9090436ed3ca37
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2014-06-20 00:17:21 +0200

    Add main Program and Local execution test
    * Still facing problems with
    java.lang.RuntimeException: java.lang.NoSuchMethodException: org.w3c.dom.Document.<init>()
    probably doc can not be used as data type...

---
+++ src/test/java/de/tuberlin/dima/schubotz/fse/IntegrationTest.java
@@ -0,0 +1,20 @@
+package de.tuberlin.dima.schubotz.fse;
+
+import eu.stratosphere.api.common.Plan;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.client.LocalExecutor;
+import org.junit.Test;
+
+public class IntegrationTest {
+    @Test
+    public void TestLocalExecution() throws Exception {
+        String inputFilename = "file://" + getClass().getClassLoader().getResources("test10.xml").nextElement().getPath();
+        String queryFile = "file://" + getClass().getClassLoader().getResources("fQuery.xml").nextElement().getPath();
+        String outputFilename = "file://" + getClass().getClassLoader().getResources("test.out").nextElement().getPath();
+        MainProgram.parseArg(new String[]{"16",queryFile,inputFilename,outputFilename});
+        MainProgram.ConfigurePlan();
+        ExecutionEnvironment env = MainProgram.getExecutionEnvironment();
+        Plan plan = env.createProgramPlan();//rc.getPlan(inputFilename, outputFilename + Math.random() * Integer.MAX_VALUE, "1.5", "0");
+        LocalExecutor.execute(plan);
+    }
+}
