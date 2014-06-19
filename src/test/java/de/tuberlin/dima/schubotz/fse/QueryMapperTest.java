654aaf611947ad94363df1819174ce0c6da6f12a
Physikerwelt <wiki@physikerwelt.de>
2014-06-19 22:24:25 +0200

    Add more tests

--- src/test/java/de/tuberlin/dima/schubotz/fse/QueryMapperTest.java
+++ src/test/java/de/tuberlin/dima/schubotz/fse/QueryMapperTest.java
@@ -2,11 +2,14 @@ package de.tuberlin.dima.schubotz.fse;
 
 import eu.stratosphere.util.Collector;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 import java.util.ArrayList;
 
-public class QueryMapperTest {
+import static junit.framework.Assert.assertEquals;
 
+public class QueryMapperTest {
     @Test
     public void testFlatMap() throws Exception {
         QueryMapper queryMapper = new QueryMapper();
@@ -14,7 +17,7 @@ public class QueryMapperTest {
         Collector<Query> col = new Collector<Query>() {
             @Override
             public void collect(Query record) {
-                System.out.println(record.name);
+                // System.out.println(record.name);
                 queries.add(record);
             }
 
@@ -24,6 +27,15 @@ public class QueryMapperTest {
             }
         };
         queryMapper.flatMap(TestUtils.getTestQueryString(), col);
-
+        Query query10 = queries.get(9);
+        assertEquals("NTCIR11-Math-10", query10.name);
+        assertEquals(1, query10.formulae.size());
+        assertEquals(4, query10.keywords.size());
+        assertEquals(true, query10.keywords.containsKey("w1.3"));
+        assertEquals("code", query10.keywords.get("w1.3"));
+        assertEquals(true, query10.formulae.containsKey("f1.0"));
+        Document testDoc = query10.formulae.get("f1.0");
+        Node texExpression = XMLHelper.getElementB(testDoc, "/math/semantics/annotation");
+        assertEquals("f(\\qvar{x})=\\frac{1}{\\sigma\\sqrt{2\\pi}}\\qvar{z}", texExpression.getTextContent().trim());
     }
 }
