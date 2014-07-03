369e1604b4b5023b1ce22f1fd28aa958a06bbf34
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-03 10:08:14 -0400

    fixed bug, also now outputs num docs to txt file

--- src/main/java/de/tuberlin/dima/schubotz/fse/preprocess/LatexDocMapper.java
+++ src/main/java/de/tuberlin/dima/schubotz/fse/preprocess/LatexDocMapper.java
@@ -1,22 +1,19 @@
 package de.tuberlin.dima.schubotz.fse.preprocess;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.apache.commons.lang.StringEscapeUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-
 import de.tuberlin.dima.schubotz.fse.LatexHelper;
 import de.tuberlin.dima.schubotz.fse.QueryTuple;
-import de.tuberlin.dima.schubotz.fse.SectionTuple;
 import de.tuberlin.dima.schubotz.fse.XMLHelper;
 import eu.stratosphere.api.java.functions.FlatMapFunction;
 import eu.stratosphere.api.java.tuple.Tuple2;
 import eu.stratosphere.configuration.Configuration;
 import eu.stratosphere.util.Collector;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LatexDocMapper extends FlatMapFunction<String, Tuple2<String,Integer>>{
 	HashSet<String> latex;
@@ -37,27 +34,33 @@ public class LatexDocMapper extends FlatMapFunction<String, Tuple2<String,Intege
 	}
 	
 	@Override
-	public void flatMap (String value, Collector<Tuple2<String,Integer>> out) throws Exception {
-		//Takes in document, outputs tuple of <latex token, 1> for every token in document contained in set of query latex
+	public void flatMap (String value, Collector<Tuple2<String,Integer>> out) {
+		//Takes in document, outputs tuple of <latex token, 1> for every document containing a token contained in set of query latex
 		//Remove <ARXIV> and <?xml
+		NodeList LatexElements;
 		String[] lines = value.trim().split( "\\n", 2 );
 		if ( lines.length < 2 ) { 
 			System.out.println("Null document (LatexDocMapper): " + value); //DEBUG output null document
 			return;
 		}
-		//Parse string as XML
-		Document doc = XMLHelper.String2Doc(lines[1], false); 
-		NodeList LatexElements = XMLHelper.getElementsB(doc, "//annotation"); //get all annotation tags
-		
+		try {
+			//Parse string as XML
+			Document doc = XMLHelper.String2Doc( lines[1], false );
+			LatexElements= XMLHelper.getElementsB( doc, "//annotation" ); //get all annotation tags
+		} catch ( Exception e ){
+			System.out.println("Could not parse document:"+lines[0] );
+			LatexElements = null;
+		}
 		//Extract latex
 		String sectionLatex = LatexHelper.extract(LatexElements);
-		String[] tokens = sectionLatex.split( "<S>" );
-		Set<String> tokenSet = new HashSet<String>(Arrays.asList(tokens)); //remove repeats
-		
-		//Loop through and output
-		for (String token : tokenSet) {
-			if (latex.contains(token)) {
-				out.collect(new Tuple2<String,Integer>(token, 1));
+		if (!sectionLatex.equals("")) {
+			String[] tokens = sectionLatex.split("<S>"); 
+			Set<String> tokenSet = new HashSet<String>(Arrays.asList(tokens)); //remove repeats (only want number of documents)
+			//Loop through and output
+			for (String token : tokenSet) {
+				if (latex.contains(token)) {
+					out.collect(new Tuple2<String,Integer>(token, 1));
+				}
 			}
 		}
 	}
--- src/test/java/de/tuberlin/dima/schubotz/fse/ProcessDataTest.java
+++ src/test/java/de/tuberlin/dima/schubotz/fse/ProcessDataTest.java
@@ -11,20 +11,23 @@ public class ProcessDataTest {
 	@Test
 	public void TestProcessData() throws Exception {
 		try {
-			String inputFilename = "file://" + getClass().getClassLoader().getResources("expectedMatch.xml").nextElement().getPath();
+			String inputFilename = "file://" + getClass().getClassLoader().getResources("test10000.xml").nextElement().getPath();
 			System.out.println("ProcessData testing on: " + inputFilename);
 			String queryFile = "file://" + getClass().getClassLoader().getResources("fQuery.xml").nextElement().getPath();
 			String keywordDocsFilename = "file://" + getClass().getClassLoader().getResources("keywordDocsMap.csv").nextElement().getPath();
 			String latexDocsFilename = "file://" + getClass().getClassLoader().getResources("latexDocsMap.csv").nextElement().getPath();
-//			//DEBUG file IO
-//			String keywordDocsFilename = "file:///home/jjl4/keywordDocsMap.csv";
-//			String latexDocsFilename = "file:///home/jjl4/latexDocsMap.csv";
+	        String numDocsFilename = "file://" + getClass().getClassLoader().getResources("numDocs.txt").nextElement().getPath();
+			//DEBUG file IO
+//			keywordDocsFilename = "file:///home/jjl4/keywordDocsMap.csv";
+//			latexDocsFilename = "file:///home/jjl4/latexDocsMap.csv";
+//			numDocsFilename = "file:///home/jjl4/numDocs.txt";
 			
 			ProcessData.parseArg(new String[]{"16",
 											  inputFilename,
 											  queryFile,
 											  keywordDocsFilename + Math.random() * Integer.MAX_VALUE,
-											  latexDocsFilename + Math.random() * Integer.MAX_VALUE});
+											  latexDocsFilename + Math.random() * Integer.MAX_VALUE,
+											  numDocsFilename});
 			ProcessData.ConfigurePlan();
 	        ExecutionEnvironment env = ProcessData.getExecutionEnvironment();
 	        Plan plan = env.createProgramPlan();
