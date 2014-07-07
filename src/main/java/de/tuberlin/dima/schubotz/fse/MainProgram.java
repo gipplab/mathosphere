4037e1e456c67fceda5885b07783d8e278515f68
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-07 15:13:56 -0400

    notes to keep eye out for global variables accessed. more general code cleanup.

--- src/main/java/de/tuberlin/dima/schubotz/fse/MainProgram.java
+++ src/main/java/de/tuberlin/dima/schubotz/fse/MainProgram.java
@@ -155,7 +155,7 @@ public class MainProgram {
 		
 		//Set up articleDataSet
 		TextInputFormat format = new TextInputFormat( new Path( docsInput ) );
-		format.setDelimiter( DOCUMENT_SEPARATOR ); //WATCH fix stratosphere split issues for Queries and Documents
+		format.setDelimiter( DOCUMENT_SEPARATOR );
 		DataSet<String> rawArticleText = new DataSource<>( env, format, BasicTypeInfo.STRING_TYPE_INFO );
 		
 		
