abb2331536aedb3e304682c1b5d6b54ee059b47c
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-10 10:31:55 -0400

    Created CSVmultiset class

--- src/wikipedia/java/de/tuberlin/dima/schubotz/wiki/WikiProgram.java
+++ src/wikipedia/java/de/tuberlin/dima/schubotz/wiki/WikiProgram.java
@@ -8,8 +8,8 @@ import org.apache.commons.logging.LogFactory;
  * Returns search results for NTCIR-11 2014 Wikipedia Subtask
  *
  */
-public class MainProgram {
-	private static final Log LOG = LogFactory.getLog(MainProgram.class);
+public class WikiProgram {
+	private static final Log LOG = LogFactory.getLog(WikiProgram.class);
 	
 	protected static void parseArg(String[] args) throws Exception {
 		
