a232c2f508de0be8efc947da5676a921abfcef06
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-14 10:59:02 -0400

    new wikituple class

--- src/wikipedia/java/de/tuberlin/dima/schubotz/wiki/types/WikiTuple.java
+++ src/wikipedia/java/de/tuberlin/dima/schubotz/wiki/types/WikiTuple.java
@@ -1,5 +1,34 @@
 package de.tuberlin.dima.schubotz.wiki.types;
 
-public class WikiTuple {
+import eu.stratosphere.api.java.tuple.Tuple2;
+
+public class WikiTuple extends Tuple2<String,String>{
+	private String split;
+	
+	public WikiTuple () {
+		this.f0 = "nulldoc";
+		this.f1 = "";
+	}
+	
+	/**
+	 * @param docID
+	 * @param latex
+	 */
+	public WikiTuple (String docID, String latex) {
+		this.f0 = docID;
+		this.f1 = latex;
+	}
+	
+	public String getID() {
+		return this.f0;
+	}
+	
+	public String getLatex() {
+		return this.f1;
+	}
+	
+	public enum fields {
+		id, latex
+	}
 
 }
