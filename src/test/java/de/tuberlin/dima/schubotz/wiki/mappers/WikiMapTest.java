bc0edc54fab08731714d0dcf5b0682d21c114bc7
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-25 14:38:28 -0400

    Switch to using CSV_LINE_SEPARATOR in tests   Easier in future to read resulting output (XML in CSV has \n)   Rename resources to CSV to better express CSV-ness

--- src/test/java/de/tuberlin/dima/schubotz/wiki/mappers/WikiMapTest.java
+++ src/test/java/de/tuberlin/dima/schubotz/wiki/mappers/WikiMapTest.java
@@ -1,5 +1,6 @@
 package de.tuberlin.dima.schubotz.wiki.mappers;
 
+import de.tuberlin.dima.schubotz.wiki.types.WikiQueryTuple;
 import de.tuberlin.dima.schubotz.wiki.types.WikiTuple;
 import eu.stratosphere.api.java.DataSet;
 import org.junit.Test;
@@ -7,20 +8,20 @@ import org.junit.Test;
 /**
  * Used to test WikiMapper on the wiki sets with dependency on wikiQuery.expected files.
  * If running tests on full wiki set, the others should be @Ignore annotated.
+ * TODO parameterize this
  */
 public class WikiMapTest extends WikiAbstractSubprocessTest {
-    @Test
     /**
      * Test on first two wikis.
      */
-    public void testQuickDatset() throws Exception {
+    @Test
+    public void testQuickDataset() throws Exception {
         String inputFilename = "de/tuberlin/dima/schubotz/wiki/mappers/wikiQuickDump.xml";
-        String queryFilename = "de/tubelrin/dima/schubotz/wiki/mappers/wikiQuickQuery.expected.xml";
-        String expectedOutputFilename = "de/tuberlin/dima/schubotz/wiki/mappers/wikiQuickDump.expected.xml";
-        DataSet<String> data = getCleanedData(inputFilename);
-        DataSet<String> queries = getCleanedData(queryFilename);
-        DataSet<WikiTuple> outputSet = data.flatMap(new WikiMapper(STR_SPLIT))
-                                          .withBroadcastSet(queries, "Queries");
+        String queryFilename = "de/tuberlin/dima/schubotz/wiki/mappers/wikiQuickQuery.expected.csv";
+        String expectedOutputFilename = "de/tuberlin/dima/schubotz/wiki/mappers/wikiQuickDump.expected.csv";
+        DataSet<String> data = (DataSet<String>) getCleanedData(inputFilename);
+        DataSet<WikiQueryTuple> queries = (DataSet<WikiQueryTuple>) getCleanedData(queryFilename);
+        DataSet<WikiTuple> outputSet = data.flatMap(new WikiMapper(STR_SPLIT));
         testDataMap(outputSet, expectedOutputFilename);
 
     }
