82be65443c2beb434586d4ee2b7afb5750cdaf3d
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-29 15:53:03 -0400

    Construct skeleton tests and methods for XML Comparison

---
+++ src/test/java/de/tuberlin/dima/schubotz/common/utils/ComparisonHelperTest.java
@@ -0,0 +1,7 @@
+package de.tuberlin.dima.schubotz.common.utils;
+
+/**
+ * Created by jjl4 on 7/29/14.
+ */
+public class ComparisonHelperTest {
+}
--- src/wikipedia/java/de/tuberlin/dima/schubotz/wiki/mappers/QueryWikiMatcher.java
+++ src/wikipedia/java/de/tuberlin/dima/schubotz/wiki/mappers/QueryWikiMatcher.java
@@ -3,12 +3,12 @@ package de.tuberlin.dima.schubotz.wiki.mappers;
 import java.util.Arrays;
 import java.util.Collection;
 
+import de.tuberlin.dima.schubotz.common.utils.ComparisonHelper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.google.common.collect.HashMultiset;
 
-import de.tuberlin.dima.schubotz.common.utils.TFIDFHelper;
 import de.tuberlin.dima.schubotz.fse.types.ResultTuple;
 import de.tuberlin.dima.schubotz.wiki.types.WikiQueryTuple;
 import de.tuberlin.dima.schubotz.wiki.types.WikiTuple;
@@ -78,32 +78,35 @@ public class QueryWikiMatcher extends FlatMapFunction<WikiTuple, ResultTuple> {
 		HashMultiset<String> queryLatex;
 		double latexScore;
 		double finalScore;
-		for (WikiQueryTuple query : queries) {
-			if (LOG.isDebugEnabled() && debug) {  
-				LOG.debug(query.toString());
-				LOG.debug(in.toString());
-				LOG.debug(Arrays.asList(in.getLatex().split(STR_SPLIT)));
-			}
+        double mmlScore;
+        double pmmlScore;
+
+        for (WikiQueryTuple query : queries) {
+            if (LOG.isDebugEnabled() && debug) {
+                LOG.debug(query.toString());
+                LOG.debug(in.toString());
+                LOG.debug(Arrays.asList(in.getLatex().split(STR_SPLIT)));
+            }
 			
-			if (!sectionLatex.isEmpty()) {
-				queryLatex = HashMultiset.create(Arrays.asList(query.getLatex().split(STR_SPLIT)));
-				latexScore = TFIDFHelper.calculateTFIDFScore(queryLatex, sectionLatex, latexWikiMultiset, numWiki, debug);
-			} else {
-				latexScore = 0.;
-			}
+            if (!sectionLatex.isEmpty()) {
+                queryLatex = HashMultiset.create(Arrays.asList(query.getLatex().split(STR_SPLIT)));
+                latexScore = ComparisonHelper.calculateTFIDFScore(queryLatex, sectionLatex, latexWikiMultiset, numWiki, debug);
+            } else {
+                latexScore = 0.;
+            }
 			
-			 finalScore = latexScore;
-			 
-			 if( Double.isNaN( finalScore )) {
-				 if (LOG.isWarnEnabled() ) {
-					 LOG.warn("NaN hit! Latex: " + Double.toString(latexScore));
-				 }
-				 finalScore = 0;
-			}
-			out.collect(new ResultTuple(query.getID(), in.getID(), finalScore));			
-			
+            if( Double.isNaN( latexScore )) {
+                if (LOG.isWarnEnabled() ) {
+                    LOG.warn("NaN hit! Latex: " + Double.toString(latexScore));
+                }
+                latexScore = 0;
+            }
+
+            mmlScore = ComparisonHelper.calculateMMLScore(in.getMML(), query.getMML());
+            pmmlScore = ComparisonHelper.calculatePMMLScore(in.getPMML(), query.getPMML());
+            finalScore = latexScore + mmlScore + pmmlScore;
+
+            out.collect(new ResultTuple(query.getID(), in.getID(), finalScore));
 		}
-
 	}
-
 }
