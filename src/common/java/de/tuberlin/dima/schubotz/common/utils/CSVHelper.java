1fa6fb5b417402d5bd296be71c93b7a8eca25d1c
jimmyli97 <jimmy.jiajian.li@gmail.com>
2014-07-31 10:46:40 -0400

    Write csv converter for wikituple   Switch to using \u01df for field seperator   Stratosphere only takes chars, not strings for field sep :(

--- src/common/java/de/tuberlin/dima/schubotz/common/utils/CSVHelper.java
+++ src/common/java/de/tuberlin/dima/schubotz/common/utils/CSVHelper.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 import de.tuberlin.dima.schubotz.wiki.WikiProgram;
 import de.tuberlin.dima.schubotz.wiki.types.WikiTuple;
 import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.io.CsvInputFormat;
+import eu.stratosphere.api.java.io.CsvReader;
+import eu.stratosphere.api.java.operators.DataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -19,7 +23,7 @@ import com.google.common.collect.HashMultiset;
 public class CSVHelper {
 	public static Log LOG = LogFactory.getLog(CSVHelper.class);
     public static final String CSV_LINE_SEPARATOR = WikiProgram.CSV_LINE_SEPARATOR;
-    public static final String CSV_FIELD_SEPARATOR = WikiProgram.CSV_FIELD_SEPARATOR;
+    public static final char CSV_FIELD_SEPARATOR = WikiProgram.CSV_FIELD_SEPARATOR.charAt(0);
 	/**
 	 * @param in path + name of file to read from
 	 * @return HashMultiset, where index is first CSV field, count is second CSV field
@@ -31,7 +35,7 @@ public class CSVHelper {
 		BufferedReader br = new BufferedReader(new FileReader(in));
         String line = "";
         while ((line = br.readLine()) != null) {
-        	String[] parts = line.split(CSV_FIELD_SEPARATOR);
+        	String[] parts = line.split(String.valueOf(CSV_FIELD_SEPARATOR));
         	try {
         		out.add(parts[0], Integer.valueOf(parts[1]).intValue());
         	} catch (NullPointerException e) {
@@ -45,7 +49,15 @@ public class CSVHelper {
         return out;
 	}
 
-    public static DataSet<WikiTuple> csvToWikiTuple(String in) {
-        return null;
+    /**
+     * Must be run in stratosphere environment
+     * @param in filename
+     * @return dataset
+     */
+    public static DataSet<WikiTuple> csvToWikiTuple(ExecutionEnvironment env, String in) {
+        CsvReader reader = env.readCsvFile(in);
+        reader.fieldDelimiter(CSV_FIELD_SEPARATOR);
+        reader.lineDelimiter(CSV_LINE_SEPARATOR);
+        return reader.tupleType(WikiTuple.class);
     }
 }
--- src/test/java/de/tuberlin/dima/schubotz/common/utils/ComparisonHelperTest.java
+++ src/test/java/de/tuberlin/dima/schubotz/common/utils/ComparisonHelperTest.java
@@ -1,18 +1,47 @@
 package de.tuberlin.dima.schubotz.common.utils;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Scanner;
+
+import static junit.framework.TestCase.assertEquals;
+
 
 /**
  * Test class for XML comparison methods.
  * This should test all methods of comparison,
  * not just the one being used.
  */
+@RunWith(Parameterized.class)
 public class ComparisonHelperTest {
+    private String prefix;
+    private double expectedScore;
+
+    /**
+     * Params in form of prefix, expected score.
+     * Prefixes are the common prefix of the test resource
+     * @return array of parameters
+     */
+    @Parameterized.Parameters
+	public static Collection<Object[]> inputNumDocs() {
+		return Arrays.asList(new Object[][]{
+                {"qvar.MML.Identical", 1.0}
+        });
+	}
+
+    public ComparisonHelperTest(String prefix, double score) {
+        this.prefix = prefix;
+        this.expectedScore = score;
+    }
+
     //TODO merge this with TestUtils' method
     private String getFileAsString(String filename) throws IOException {
         InputStream resource = ComparisonHelperTest.class.getClassLoader().getResourceAsStream(filename);
@@ -29,21 +58,12 @@ public class ComparisonHelperTest {
             resource.close();
         }
     }
-    @Test
-    public void testQvarDifferent() {
-
-    }
 
     @Test
-    public void testQvarSimilar() {
-
-    }
-    @Test
-    public void testQvarIdentical() {
-
-    }
-    @Test
-    public void testSubtreeIdentical() {
-
+    public void testComparison() throws IOException {
+        String in = getFileAsString(prefix + ".xml");
+        String compare = getFileAsString(prefix + ".compare.xml");
+        double score = ComparisonHelper.calculateMMLScore(in, compare);
+        assertEquals(score, expectedScore);
     }
 }
