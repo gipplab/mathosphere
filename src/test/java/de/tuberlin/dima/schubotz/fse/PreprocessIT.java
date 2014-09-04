e1887e74d680caa7ff46876f310a581e6c699f90
makecakenotwar97@gmail.com <makecakenotwar97@gmail.com>
2014-09-03 23:20:52 -0400

    Fix bug in integration test with backslashes Still unable to write (throws FileNotFound)

--- src/test/java/de/tuberlin/dima/schubotz/fse/PreprocessIT.java
+++ src/test/java/de/tuberlin/dima/schubotz/fse/PreprocessIT.java
@@ -7,11 +7,12 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.io.File;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class PreprocessIT {
@@ -27,8 +28,28 @@ public class PreprocessIT {
                 TestUtils.getFQueryStringPath()).nextElement().getPath();
         final String runtag = "mainTest10";
         final String numSubTasks = "1";
-        final File outputFileFolder = testFolder.newFolder("testMain10");
-        final String outputDir = "file://" + outputFileFolder.getCanonicalPath();
+        File[] files = {testFolder.newFolder("testMain10"),
+                testFolder.newFile("preprocessedData.csv"),
+                testFolder.newFile("preprocessedQueries.csv"),
+                testFolder.newFile("latexCounts.csv"),
+                testFolder.newFile("numDocs.csv"),
+                testFolder.newFile("keywordCounts.csv")};
+        for (File file : files) {
+            file.setWritable(true);
+            if (!file.canWrite() || !file.canRead()) {
+                fail("Unable to write to files, test cannot be run.");
+                return;
+            };
+        }
+
+        String outputDir = testFolder.getRoot().getCanonicalPath();
+        outputDir = outputDir.replaceAll("\\\\","/");
+
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(outputDir + "/" + "numDocs.csv"))) {
+            out.write("asdfasdf");
+        } catch (Exception e) {
+            fail("Could not write to numDocs: " + outputDir + " " + e.getMessage());
+        }
 
         final String[] params = {algoModule, inputModule, "-DATARAW_FILE", rawDataFile,
                 "-QUERY_FILE", rawQueryFile, "-RUNTAG", runtag,
@@ -36,7 +57,12 @@ public class PreprocessIT {
 
         MainProgram.main(params);
 
-        assertEquals(2, (long) outputFileFolder.listFiles().length);
+        for (File file : files) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                br.readLine();
+                assertTrue(br.readLine().length() > 0);
+            }
+        }
 
     }
 }
