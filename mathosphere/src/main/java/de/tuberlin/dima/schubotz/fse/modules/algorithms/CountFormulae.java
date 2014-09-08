dee0a4ed1a535ce54d1ad24b8d403c6f172af75d
physikerwelt <wiki@physikerwelt.de>
2014-09-08 18:47:15 -0400

    Update to apache flink 0.7
    We need a custom build to fix problems with
     * the CLI options
     * database writes
    
    Change-Id: Iad132d973e19a576a924cce138be0ce2724c9ab0

--- mathosphere/src/main/java/de/tuberlin/dima/schubotz/fse/modules/algorithms/CountFormulae.java
+++ mathosphere/src/main/java/de/tuberlin/dima/schubotz/fse/modules/algorithms/CountFormulae.java
@@ -5,38 +5,15 @@ import de.tuberlin.dima.schubotz.fse.settings.SettingNames;
 import de.tuberlin.dima.schubotz.fse.settings.Settings;
 import de.tuberlin.dima.schubotz.fse.types.RawDataTuple;
 import de.tuberlin.dima.schubotz.fse.utils.SafeLogWrapper;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.jdbc.JDBCOutputFormat;
 import org.apache.flink.api.java.operators.FlatMapOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 
-import java.util.Collection;
 
-
-public class CountFormulae implements Algorithm  {
+public class CountFormulae extends SimpleDbMapper {
     private static final SafeLogWrapper LOG = new SafeLogWrapper(CountFormulae.class);
-	//TODO: Make this configurable as other settings too
-	private final static String DBURL = "jdbc:mysql://localhost:3306/mathosphere";
-	public static final String DRIVERNAME = "org.mariadb.jdbc.Driver";
-	public static final String USER = "mathosphere";
-    private static final Option PASSWORD= new Option(
-            SettingNames.PASSWORD.getLetter(), SettingNames.PASSWORD.toString(), true,
-            "Password for mysql");
-    private static final Options MainOptions = new Options();
-
-    static {
-        //Load command line options here
-        PASSWORD.setRequired(true);
-        PASSWORD.setArgName("password");
-        MainOptions.addOption(PASSWORD);
-    }
-    @Override
-    public Collection<Option> getOptionsAsIterable() {
-        return MainOptions.getOptions();
-    }
 
 
 
