7393166915767e92223310a2890ccc86941e235e
physikerwelt <wiki@physikerwelt.de>
2014-09-10 18:52:13 -0400

    Math Similarity Factors
    * Local tests to demonstrate the principals for
      math similarity search
    * TODO: Currenly only local junit tests. Needs to be
      transformed to a flink dataflow
    
    Update lib, --fix "Error Loading Project"--
    --Removed flink folders from showing up as IDEA modules--
    Update lib to latest commits
    
    Patch 8: fix MainMapper logging, fix MainMapper test
    
    Patch 9: refactor ArxivCleaner/UltimateDataTest, new GenericCollector for testing
    
    Patch 10: add ssh link instructions to readme
    
    Patch 11: make class names consistent
    
    Patch 12: (cont. patch 11) - I forgot to turn on ignorecase
    
    Patch 13: reorganize preprocess package
    
    Patch 14: along with patch 9, refactor ArxivCleanerTest
    
    Patch 15: (cont. patch 14), move test into cleaners package
    
    Patch 16: fix logging in ToStrict
    
    Change-Id: I7974798da5284987cb62014d6ab38fc62bed47c4

--- mathosphere/src/test/java/de/tuberlin/dima/schubotz/utils/TestUtils.java
+++ mathosphere/src/test/java/de/tuberlin/dima/schubotz/utils/TestUtils.java
@@ -1,12 +1,20 @@
 package de.tuberlin.dima.schubotz.utils;
 
+import de.tuberlin.dima.schubotz.fse.settings.SettingNames;
+import de.tuberlin.dima.schubotz.fse.settings.Settings;
+import de.tuberlin.dima.schubotz.fse.utils.XMLHelper;
 import org.apache.flink.core.fs.Path;
+import org.junit.Assume;
+import org.w3c.dom.Node;
 
 import java.io.*;
+import java.sql.*;
 import java.util.Scanner;
 
 public class TestUtils {
-    public static String getTestQueryString() throws IOException {
+	private static Connection cn = null;
+
+	public static String getTestQueryString() throws IOException {
         return getFileContents("de/tuberlin/dima/schubotz/fse/fQuery.xml");
     }
 
@@ -34,8 +42,8 @@ public class TestUtils {
     	return new Path("de/tuberlin/dima/schubotz/fse/test10.xml");
     }
 
-    public static String getFileContents(String fname) throws IOException {
-        final InputStream is = TestUtils.class.getClassLoader().getResourceAsStream(fname);
+    public static String getFileContents(String fName) throws IOException {
+        final InputStream is = getClassLoader().getResourceAsStream(fName);
         try {
             final Scanner s = new Scanner(is, "UTF-8");
             //Stupid scanner tricks to read the entire file as one token
@@ -45,7 +53,23 @@ public class TestUtils {
             is.close();
         }
     }
-    
+
+	public static String setTestPassword (){
+		if (getClassLoader().getResource( "testpassword") == null){
+			Assume.assumeTrue( false );
+		}
+		String testPassword = null;
+		try {
+			testPassword = getFileContents( "testpassword" );
+		} catch ( IOException e ) {
+			e.printStackTrace();
+			Assume.assumeTrue( false );
+		}
+		Assume.assumeFalse( testPassword == null );
+		Settings.setProperty( SettingNames.PASSWORD, testPassword );
+		return testPassword;
+	}
+
     public static int countLines(String filename) throws IOException {
     	InputStream is = new BufferedInputStream(new FileInputStream(filename));
     	try {
@@ -71,4 +95,43 @@ public class TestUtils {
     		is.close();
     	}
     }
+
+	public static void setTestQueries () {
+		String fName = getClassLoader().
+			getResource( "de/tuberlin/dima/schubotz/fse/fQuery.xml" ).toString();
+		Settings.setProperty( SettingNames.QUERY_FILE, fName );
+	}
+
+	public static ClassLoader getClassLoader () {
+		return TestUtils.class.getClassLoader();
+	}
+
+	static void dPring (Node n){
+		String s = XMLHelper.CompactForm( n );
+		s=s.replace( "#document[math[semantics[", "\\Tree[ " );
+		s=s.replace( ";", " " );
+		s=s.substring( 0,s.length()-2 );
+		System.out.println( StringAugmentTree(s) );
+	}
+	private static String StringAugmentTree(String t){
+		return t.replace( "apply[times" , "[.$*$" )
+			.replace( "apply[plus" , "[.$+$" )
+			.replace( "apply[minus" , "[.$-$" )
+			.replace( "apply[eq" , "[.$=$" )
+			.replace( "apply[divide" , "[.$/$" )
+			.replace( "apply[leq" , "[.$\\leq$" )
+			.replaceAll( "ci\\[(\\w)\\]","\\$$1\\$" )
+			.replaceAll( "cn\\[(\\d+)\\]","\\$$1\\$" )
+			.replaceAll( "apply\\[(\\w+)","[.@ $1")
+			.replaceAll( "(\\w+\\d?)\\[(\\w+)","[.$1 $2")
+			.replace( "]"," ] " );
+	}
+
+	public static Connection getConnection () throws SQLException, IOException {
+		if (cn == null) {
+			cn = DriverManager.getConnection( "jdbc:mysql://localhost:3306/mathosphere",
+				"mathosphere", getFileContents( "testpassword" ) );
+		}
+		return cn;
+	}
 }
