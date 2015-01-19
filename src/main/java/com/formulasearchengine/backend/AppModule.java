00ce014e406142d80daa88c66675aa30a87f95d0
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-01-19 11:43:47 +0100

    Initial commit
    Automatically generated restd interface with restx.
    
    Change-Id: I2285589402f97ebdb5d28e8ce677603b717b04a4

---
+++ src/main/java/com/formulasearchengine/backend/AppModule.java
@@ -0,0 +1,74 @@
+package com.formulasearchengine.backend;
+
+import restx.config.ConfigLoader;
+import restx.config.ConfigSupplier;
+import restx.factory.Provides;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import restx.security.*;
+import restx.factory.Module;
+import restx.factory.Provides;
+import javax.inject.Named;
+
+import java.nio.file.Paths;
+
+@Module
+public class AppModule {
+    @Provides
+    public SignatureKey signatureKey() {
+         return new SignatureKey("3228323774839763938 fse-backend-restd 0390fce5-4811-4cd0-9aa7-ed843e0f6218 restd".getBytes(Charsets.UTF_8));
+    }
+
+    @Provides
+    @Named("restx.admin.password")
+    public String restxAdminPassword() {
+        return "9654";
+    }
+
+    @Provides
+    public ConfigSupplier appConfigSupplier(ConfigLoader configLoader) {
+        // Load settings.properties in com.formulasearchengine.backend package as a set of config entries
+        return configLoader.fromResource("com/formulasearchengine/backend/settings");
+    }
+
+    @Provides
+    public CredentialsStrategy credentialsStrategy() {
+        return new BCryptCredentialsStrategy();
+    }
+
+    @Provides
+    public BasicPrincipalAuthenticator basicPrincipalAuthenticator(
+            SecuritySettings securitySettings, CredentialsStrategy credentialsStrategy,
+            @Named("restx.admin.passwordHash") String defaultAdminPasswordHash, ObjectMapper mapper) {
+        return new StdBasicPrincipalAuthenticator(new StdUserService<>(
+                // use file based users repository.
+                // Developer's note: prefer another storage mechanism for your users if you need real user management
+                // and better perf
+                new FileBasedUserRepository<>(
+                        StdUser.class, // this is the class for the User objects, that you can get in your app code
+                        // with RestxSession.current().getPrincipal().get()
+                        // it can be a custom user class, it just need to be json deserializable
+                        mapper,
+
+                        // this is the default restx admin, useful to access the restx admin console.
+                        // if one user with restx-admin role is defined in the repository, this default user won't be
+                        // available anymore
+                        new StdUser("admin", ImmutableSet.<String>of("*")),
+
+                        // the path where users are stored
+                        Paths.get("data/users.json"),
+
+                        // the path where credentials are stored. isolating both is a good practice in terms of security
+                        // it is strongly recommended to follow this approach even if you use your own repository
+                        Paths.get("data/credentials.json"),
+
+                        // tells that we want to reload the files dynamically if they are touched.
+                        // this has a performance impact, if you know your users / credentials never change without a
+                        // restart you can disable this to get better perfs
+                        true),
+                credentialsStrategy, defaultAdminPasswordHash),
+                securitySettings);
+    }
+}
