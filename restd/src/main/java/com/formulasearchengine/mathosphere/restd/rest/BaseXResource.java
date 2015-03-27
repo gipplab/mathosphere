c44ff17f95d67c36510e33a0692f5b9a9603342c
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-03-27 01:22:53 -0400

    Support delete, count
    * improve delete functionality
    * count interface for individual revisions and
    * total number of entries in the db
    
    Change-Id: I7aadb8fae887a6437d2811588faa52575485c8d9

--- restd/src/main/java/com/formulasearchengine/mathosphere/restd/rest/BaseXResource.java
+++ restd/src/main/java/com/formulasearchengine/mathosphere/restd/rest/BaseXResource.java
@@ -1,5 +1,6 @@
 package com.formulasearchengine.mathosphere.restd.rest;
 
+import com.formulasearchengine.mathosphere.basex.Client;
 import com.formulasearchengine.mathosphere.restd.domain.MathRequest;
 import com.formulasearchengine.mathosphere.restd.domain.MathUpdate;
 import restx.annotations.GET;
@@ -56,5 +57,17 @@ public class BaseXResource {
 	@PermitAll
 	public MathUpdate update( MathUpdate u ) {
 		return u.run();
+	}
+	@GET("/cntRev")
+	@PermitAll
+	public Integer dbsize( Integer revision ) {
+		Client client = new Client();
+		return client.countRevisionFormula( revision  );
+	}
+	@GET("/cntAll")
+	@PermitAll
+	public Integer dbsize(  ) {
+		Client client = new Client();
+		return client.countAllFormula();
 	}
 }
