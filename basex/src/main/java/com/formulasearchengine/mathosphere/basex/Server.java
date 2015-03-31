5ea7b04ffe1eb8b63ddc63a7aab01dc741c74f2d
physikerwelt <wiki@physikerwelt.de>
2015-03-31 17:48:25 -0400

    Synchronize access to  baseX server
    There must be only one baseX server instance.
    
    Change-Id: Iefdd65d18ea1dbdbc5c71fd8688fb1d44806bf6b

--- basex/src/main/java/com/formulasearchengine/mathosphere/basex/Server.java
+++ basex/src/main/java/com/formulasearchengine/mathosphere/basex/Server.java
@@ -6,83 +6,119 @@ import org.basex.BaseXServer;
 import org.basex.api.client.ClientQuery;
 import org.basex.api.client.ClientSession;
 import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.DropDB;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 /**
  * Created by Moritz on 08.11.2014.
  */
 public class Server {
+	private File file;
+	@Nullable
 	private static volatile Server instance;
+
 	/**
 	 * Get the only instance of this class.
 	 *
 	 * @return the single instance.
 	 */
-	public static Server getInstance() throws IOException {
-		if (instance == null) {
-			synchronized (Server.class) {
-				if (instance == null) {
-					instance = new Server();
-				}
+	public static Server getInstance( File f ) throws IOException {
+		synchronized ( Server.class ) {
+			if ( instance != null ) {
+				instance.shutdown();
 			}
+			instance = new Server( f );
+			return instance;
 		}
-		return instance;
+	}
+
+	@Nullable
+	public static Server getInstance() {
+		synchronized ( Server.class ) {
+			return instance;
+		}
 	}
 
 	//Do not allow for multiple server instances
+	@Nullable
 	public BaseXServer server = null;
+	@Nullable
 	public ClientSession session;
-	public boolean empty = true;
 	ServerMonitor monitor;
 
-	public static boolean isEmpty () {
-		if (instance == null){
-			return true;
-		} else {
-			try {
-				return getInstance().empty;
-			} catch ( IOException e ) {
-				e.printStackTrace();
-				return false;
-			}
-		}
+
+	public Server(File input) throws IOException {
+		startup( );
+		file = input;
+		importDataFromFile( );
 	}
 
-	public Server() throws IOException {
-			server = new BaseXServer( "-d" );
-			session = new ClientSession( server.context, "admin", "admin" );
+	private void startup() throws IOException {
+		server = new BaseXServer( ); // "-d" for debug
+		session = new ClientSession( server.context, "admin", "admin" );
 	}
 
 	public void shutdown() throws IOException {
-		monitor.shutdown();
-		server.stop();
+		if (monitor !=null ){
+			monitor.shutdown();
+		}
+		if (session !=null){
+			session.execute( "CLOSE" );
+			DropDB db = new DropDB( "math" );
+			db.execute( server.context );
+			session.close();
+		}
+		if (server != null){
+			server.stop();
+		}
 		server = null;
 		session = null;
-		empty = true;
 		instance = null;
+		ensureFileLockRelease();
+		System.gc();
 	}
 
-	public void importData( String path ) throws IOException {
+	private void ensureFileLockRelease() {
+		System.out.println( "Server could not be started. Try to lock database source file.");
+		FileChannel channel = null;
+		try {
+			channel = new RandomAccessFile( file, "rw" ).getChannel();
+		} catch ( FileNotFoundException e ) {
+			System.out.println( "File is gone.");
+			e.printStackTrace();
+			return;
+		}
+		FileLock lock = null;
+		try {
+			lock = channel.lock();
+			System.out.println("File locked");
+			// make sure that org.basex.io.random.TableDiskAccess.locked will find an unlocked file
+			lock.release();
+			System.out.println( "File unlocked." );
+		} catch ( IOException e ) {
+			System.out.println("File could not be unlocked");
+			e.printStackTrace();
+		}
+	}
+
+	private boolean importDataFromFile( ) throws IOException {
 		session.execute( "SET mainmem true" );
-		session.execute( "SET DEBUG true" );
+		//session.execute( "SET DEBUG true" );
 		session.execute( "SET SERIALIZER newline=\"\\n\"" );
 		session.execute( "SET SERIALIZER item-separator=\"\\n\"" );
-		try {
-			File f = new File( path );
-			if ( f.isFile() ) {
-				path = Files.toString( f, Charsets.UTF_8 );
-			}
-		} catch ( Exception ignored ) {	}
-		CreateDB db = new CreateDB( "math", path );
+		String fname = Files.toString( file, Charsets.UTF_8 );
+		CreateDB db = new CreateDB( "math", fname );
 		db.execute( server.context );
+		db=null;
 		System.out.println( "Import completed. Start Monitoring.");
-		//session.execute( db );
-		empty = false;
+		session.execute( "OPEN math" );
 		monitor = new ServerMonitor();
 		monitor.start();
+		return true;
 	}
 
 	public void runQuery( String queryString, PrintStream output ) throws IOException {
