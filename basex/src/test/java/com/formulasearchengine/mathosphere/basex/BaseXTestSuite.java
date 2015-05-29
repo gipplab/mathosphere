c1ea70567254d69475141752e5693981fe9638d3
jimmyli97 <jimmy.jiajian.li@gmail.com>
2015-05-29 15:24:14 -0400

    Resolve issues with Server class
    * Fix concurrency bug (resolves issue #46)
    * Fix shutdown of ServerMonitor bug (resolves issue #45)
    
    Change-Id: Ibf5c886abeb742ec871e6fe139f2246297f7c158

--- basex/src/test/java/com/formulasearchengine/mathosphere/basex/BaseXTestSuite.java
+++ basex/src/test/java/com/formulasearchengine/mathosphere/basex/BaseXTestSuite.java
@@ -12,14 +12,16 @@ import java.net.URL;
  */
 @RunWith( Suite.class )
 @Suite.SuiteClasses({ServerTest.class,ClientTest.class,BenchmarkTest.class,
-                     TexQueryGeneratorTest.class,XMLHelperTest.class,ServerMonitorTest.class})
+                     TexQueryGeneratorTest.class,XMLHelperTest.class})
 public class BaseXTestSuite {
 	@BeforeClass
 	public static void setup() throws Exception {
 		if (Server.getInstance()==null){
 			final URL fname = BaseXTestSuite.class.getClassLoader().getResource( "sampleHarvest.xml" );
 			File file = new File( fname.toURI() );
-			Server.getInstance( file );
+			Server srv = Server.getInstance();
+			srv.startup(file);
+
 		}
 	}
 }
