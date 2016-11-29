5eea4b1f289760f81740ab6dd79addc83700345b
felix <felixhamborg@gmail.com>
2016-11-29 18:23:46 +0100

    added try catch to handel this problem temporarily
    close #118

--- mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mathpd/FlinkPd.java
+++ mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mathpd/FlinkPd.java
@@ -8,7 +8,10 @@ import com.formulasearchengine.mathosphere.mlp.contracts.JsonSerializerMapper;
 import com.formulasearchengine.mathosphere.mlp.contracts.TextAnnotatorMapper;
 import com.formulasearchengine.mathosphere.mlp.pojos.ParsedWikiDocument;
 import com.formulasearchengine.mathosphere.mlp.pojos.WikiDocumentOutput;
+import com.formulasearchengine.mathosphere.mml.CMMLInfo;
 import com.google.common.collect.Multiset;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.typeutils.base.array.StringArraySerializer;
@@ -21,7 +24,11 @@ import org.apache.flink.core.fs.FileSystem.WriteMode;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.util.Collector;
 
+import javax.xml.xpath.XPathExpressionException;
+import java.util.logging.Logger;
+
 public class FlinkPd {
+    protected static final Log LOG = LogFactory.getLog(FlinkPd.class);
 
     public static void main(String[] args) throws Exception {
         FlinkPdCommandConfig config = FlinkPdCommandConfig.from(args);
@@ -38,9 +45,13 @@ public class FlinkPd {
                     @Override
                     public void reduce(Iterable<Tuple2<ArxivDocument, ArxivDocument>> iterable, Collector<Tuple2<Integer, String>> collector) throws Exception {
                         for (Tuple2<ArxivDocument, ArxivDocument> i : iterable) {
-                            final Multiset<String> elements = i.f1.getCElements();
-                            elements.removeAll(i.f0.getCElements());
-                            collector.collect(new Tuple2(elements.size(), i.f1.title + "-" + i.f0.title));
+                            try {
+                                final Multiset<String> elements = i.f1.getCElements();
+                                elements.removeAll(i.f0.getCElements());
+                                collector.collect(new Tuple2(elements.size(), i.f1.title + "-" + i.f0.title));
+                            }catch( XPathExpressionException xPathExpressionException){
+                                LOG.error("could not parse document: "+i.f0.title + " OR "+i.f1.title, xPathExpressionException);
+                            }
                         }
                     }
                 })
