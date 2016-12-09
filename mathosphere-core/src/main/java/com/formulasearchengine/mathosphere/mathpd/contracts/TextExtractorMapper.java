cbe98d4be8c2307aa269ce4c83ad460eb799c19c
felix <felixhamborg@gmail.com>
2016-12-09 17:38:27 +0100

    add test environment for distances dev

--- mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mathpd/contracts/TextExtractorMapper.java
+++ mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mathpd/contracts/TextExtractorMapper.java
@@ -4,13 +4,15 @@ import com.formulasearchengine.mathosphere.mathpd.Distances;
 import com.formulasearchengine.mathosphere.mathpd.pojos.ArxivDocument;
 import com.formulasearchengine.mathosphere.mathpd.pojos.ExtractedMathPDDocument;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,33 +23,61 @@ public class TextExtractorMapper implements FlatMapFunction<String, ExtractedMat
     private static final Pattern TITLE_PATTERN = Pattern.compile("<ARXIVFILESPLIT(?:\\\\n?|[\\s\\r\\n]+)" +
             "Filename=\"(.*?).xhtml\">(?:\\s*)(.*)", Pattern.DOTALL);
 
-
-    @Override
-    public void flatMap(String content, Collector<ExtractedMathPDDocument> out) throws Exception {
+    public static Tuple2<String, String> getTitleAndTextualContent(String content) {
         Matcher titleMatcher = TITLE_PATTERN.matcher(content);
         if (!titleMatcher.find()) {
-            return;
+            return null;
         }
-
         final String title = titleMatcher.group(1);
         final String xhtml = titleMatcher.group(2);
-        final ArxivDocument document = new ArxivDocument(title, xhtml);
-        final ExtractedMathPDDocument extractedMathPDDocument = new ExtractedMathPDDocument(title, xhtml);
 
-        LOGGER.info("processing document '{}'...", title);
+        return new Tuple2<>(title, xhtml);
+    }
+
+    public static ExtractedMathPDDocument convertArxivToExtractedMathPDDocument(ArxivDocument document) throws ParserConfigurationException, IOException, XPathExpressionException, TransformerException {
+        if (document == null)
+            return null;
+
+        final ExtractedMathPDDocument extractedMathPDDocument = new ExtractedMathPDDocument(document.title, document.text);
 
         // extract all features we are or might be interested in later
         extractedMathPDDocument.setHistogramCn(Distances.getDocumentHistogram(document, "cn"));
         extractedMathPDDocument.setHistogramCo(Distances.getDocumentHistogram(document, "co"));
         extractedMathPDDocument.setHistogramCi(Distances.getDocumentHistogram(document, "ci"));
 
+        return extractedMathPDDocument;
+    }
+
+    public static ArxivDocument arxivTextToDocument(String content) {
+        final Tuple2<String, String> titleAndContent = getTitleAndTextualContent(content);
+        if (titleAndContent == null) {
+            return null;
+        }
+
+        return new ArxivDocument(titleAndContent.f0, titleAndContent.f1);
+    }
+
+    @Override
+    public void flatMap(String content, Collector<ExtractedMathPDDocument> out) throws Exception {
+        System.out.println(content);
+        System.out.println();
+        System.out.println();
+        final ArxivDocument document = arxivTextToDocument(content);
+        if (document == null) {
+            return;
+        }
+
+        LOGGER.info("processing document '{}'...", document.title);
+        final ExtractedMathPDDocument extractedMathPDDocument = convertArxivToExtractedMathPDDocument(document);
+
+
         // write to storage for test later
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("test_title")));
-        oos.writeObject(extractedMathPDDocument);
-        oos.close();
+        //ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("test_title")));
+        //oos.writeObject(extractedMathPDDocument);
+        //oos.close();
 
         // store the doc in the collector
-        LOGGER.info("finished processing document '{}'...", title);
+        LOGGER.info("finished processing document '{}'...", document.title);
         out.collect(extractedMathPDDocument);
     }
 }
