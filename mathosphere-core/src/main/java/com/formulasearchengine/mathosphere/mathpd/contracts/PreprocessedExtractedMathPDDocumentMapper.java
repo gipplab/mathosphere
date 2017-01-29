9febe9a5b860514582fe3dece0fa2ceca8a9ec4c
felix <felixhamborg@gmail.com>
2017-01-29 18:41:45 +0100

    add debug msg

--- mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mathpd/contracts/PreprocessedExtractedMathPDDocumentMapper.java
+++ mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mathpd/contracts/PreprocessedExtractedMathPDDocumentMapper.java
@@ -5,6 +5,8 @@ import com.google.gson.Gson;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -15,9 +17,11 @@ import java.util.Base64;
  */
 public class PreprocessedExtractedMathPDDocumentMapper implements FlatMapFunction<String, Tuple2<String, ExtractedMathPDDocument>> {
     public static final Charset CHARSET = StandardCharsets.UTF_8;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreprocessedExtractedMathPDDocumentMapper.class);
 
     public static ExtractedMathPDDocument readExtractedMathPDDocumentFromText(String text) {
         final String json = new String(Base64.getDecoder().decode(text), CHARSET);
+        LOGGER.info(json);
         return new Gson().fromJson(json, ExtractedMathPDDocument.class);
     }
 
