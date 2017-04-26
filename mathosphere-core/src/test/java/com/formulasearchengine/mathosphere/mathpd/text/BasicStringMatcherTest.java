30cc5dba9c8abff9601ad4cf418164d4d5a7328d
Moritz Schubotz <wiki@physikerwelt.de>
2017-04-26 16:52:53 +0200

    Add string matching
    * add scoring function

--- mathosphere-core/src/test/java/com/formulasearchengine/mathosphere/mathpd/text/BasicStringMatcherTest.java
+++ mathosphere-core/src/test/java/com/formulasearchengine/mathosphere/mathpd/text/BasicStringMatcherTest.java
@@ -1,6 +1,11 @@
 package com.formulasearchengine.mathosphere.mathpd.text;
 
-import java.util.List;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,15 +14,31 @@ import static org.junit.Assert.assertEquals;
  */
 public class BasicStringMatcherTest {
 
-    private final String s1 = "This is the unique string one with more than six words.";
-    private final String s2 = "This is the unique string   one with more than six words.";
+    private final Map<Integer, String> testCases;
+    private final BasicStringMatcher matcher = new BasicStringMatcher(2, 4);
 
+    public BasicStringMatcherTest() {
+        testCases = new java.util.HashMap<>();
+        testCases.put(1, "word word word");
+        testCases.put(2, "word   öäüßß\0 word word");
+        testCases.put(3, "word not a word");
+    }
 
     @org.junit.Test
     public void compare() throws Exception {
-        final BasicStringMatcher matcher = new BasicStringMatcher();
-        final List<int[]> ints = matcher.compare(s1, s2);
-        assertEquals(5, ints.size());
+        Map<Tuple2<Integer, Integer>, Integer> expectedMatchLengths = new HashMap<>();
+        expectedMatchLengths.put(Tuple2.of(1, 1), 1);
+        expectedMatchLengths.put(Tuple2.of(1, 2), 1);
+        expectedMatchLengths.put(Tuple2.of(1, 3), 0);
+        Function<Tuple2<Integer, Integer>, Integer> f = entry -> {
+            try {
+                return matcher.compare(testCases.get(entry.f0), testCases.get(entry.f1)).size();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return -1;
+            }
+        };
+        runTest(expectedMatchLengths, f);
     }
 
     @org.junit.Test
@@ -35,10 +56,31 @@ public class BasicStringMatcherTest {
 
     }
 
+    private <T> void runTest(Map<Tuple2<Integer, Integer>, T> expected, Function<Tuple2<Integer, Integer>, T> f) throws Exception {
+        for (Entry<Tuple2<Integer, Integer>, T> entry : expected.entrySet()) {
+            final String message = "Test tuple " + entry.getKey().f0 + ", " + entry.getKey().f1;
+            if (entry.getValue() instanceof Double) {
+                assertEquals(message, (Double) entry.getValue(), (Double) f.apply(entry.getKey()), 0.1);
+            } else {
+                assertEquals(message, entry.getValue(), f.apply(entry.getKey()));
+            }
+        }
+    }
+
     @org.junit.Test
     public void score() throws Exception {
-        final BasicStringMatcher matcher = new BasicStringMatcher();
-        final double ints = matcher.scoreSimilarity(s1, s2);
-        assertEquals(1, ints, .01);
+        Map<Tuple2<Integer, Integer>, Double> expectedMatchLengths = new HashMap<>();
+        expectedMatchLengths.put(Tuple2.of(1, 1), 0.6);
+        expectedMatchLengths.put(Tuple2.of(1, 2), 0.6);
+        expectedMatchLengths.put(Tuple2.of(1, 3), 0.0);
+        Function<Tuple2<Integer, Integer>, Double> f = entry -> {
+            try {
+                return matcher.scoreSimilarity(testCases.get(entry.f0), testCases.get(entry.f1));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return -1.;
+            }
+        };
+        runTest(expectedMatchLengths, f);
     }
 }
