cfa9c1fef4bcbce0a652020d07bd2fdac066543f
Moritz Schubotz <wiki@physikerwelt.de>
2017-08-27 16:47:15 +0900

    Fix: Repair broken cli tests

--- mathosphere-core/src/test/java/com/formulasearchengine/mathosphere/mlp/cli/CliParamsTest.java
+++ mathosphere-core/src/test/java/com/formulasearchengine/mathosphere/mlp/cli/CliParamsTest.java
@@ -8,49 +8,54 @@ import static org.junit.Assert.assertTrue;
 
 public class CliParamsTest {
 
-  @Test
-  public void help() {
-    String[] args = {"help"};
-    CliParams params = CliParams.from(args);
-    assertEquals("help", params.getCommand());
-  }
+    @Test
+    public void count() {
+        String[] args = {"count", "--formulae", "-in", "c:/tmp/mlp/input/", "-out", "c:/tmp/mlp/out/"};
+        CliParams params = CliParams.from(args);
+        assertEquals("count", params.getCommand());
 
+        CountCommandConfig count = params.getCount();
 
-  @Test
-  public void texvcinfo() {
-    String[] args = {"tags", "--texvcinfo", "expected"};
-    CliParams params = CliParams.from(args);
-    assertEquals("expected", params.getTagsCommandConfig().getTexvcinfoUrl());
-  }
+        assertTrue(count.isFormulas());
+        assertFalse(count.isDefinitions());
+        assertFalse(count.isIdentifiers());
+        assertEquals("c:/tmp/mlp/input/", count.getInput());
+    }
 
-  @Test
-  public void useTex() {
-    String[] args = {"mlp", "--tex"};
-    CliParams params = CliParams.from(args);
-    assertEquals("mlp", params.getCommand());
-    assertTrue(params.getMlpCommandConfig().getUseTeXIdentifiers());
-  }
+    @Test
+    public void help() {
+        String[] args = {"help"};
+        CliParams params = CliParams.from(args);
+        assertEquals("help", params.getCommand());
+    }
 
-  @Test
-  public void notUseTex() {
-    String[] args = {"mlp"};
-    CliParams params = CliParams.from(args);
-    assertEquals("mlp", params.getCommand());
-    assertFalse(params.getMlpCommandConfig().getUseTeXIdentifiers());
-  }
+    @Test
+    public void notUseTex() {
+        String[] args = {"mlp", "-in", "c:/tmp/mlp/input/", "-out", "c:/tmp/mlp/out/"};
+        CliParams params = CliParams.from(args);
+        assertEquals("mlp", params.getCommand());
+        assertFalse(params.getMlpCommandConfig().getUseTeXIdentifiers());
+    }
 
-  @Test
-  public void count() {
-    String[] args = {"count", "--formulae", "-in", "c:/tmp/mlp/input/"};
-    CliParams params = CliParams.from(args);
-    assertEquals("count", params.getCommand());
+    @Test
+    public void texvcinfo() {
+        String[] args = {"tags", "--texvcinfo", "expected", "-in", "c:/tmp/mlp/input/", "-out", "c:/tmp/mlp/out/"};
+        CliParams params = CliParams.from(args);
+        assertEquals("expected", params.getTagsCommandConfig().getTexvcinfoUrl());
+    }
 
-    CountCommandConfig count = params.getCount();
+    @Test
+    public void useTex() {
+        String[] args = {"mlp", "--tex", "-in", "c:/tmp/mlp/input/", "-out", "c:/tmp/mlp/out/"};
+        CliParams params = CliParams.from(args);
+        assertEquals("mlp", params.getCommand());
+        assertTrue(params.getMlpCommandConfig().getUseTeXIdentifiers());
+    }
 
-    assertTrue(count.isFormulas());
-    assertFalse(count.isDefinitions());
-    assertFalse(count.isIdentifiers());
-    assertEquals("c:/tmp/mlp/input/", count.getInput());
-  }
-
+    @Test
+    public void voidTest() {
+        String[] args = {};
+        CliParams params = CliParams.from(args);
+        assertEquals("help", params.getCommand());
+    }
 }
