04dea5751e6497319a01f9cff8c870b7a60fb370
Moritz Schubotz <wiki@physikerwelt.de>
2017-08-27 17:15:43 +0900

    Fix: Repair more broken cli tests

--- mathosphere-core/src/test/java/com/formulasearchengine/mathosphere/mlp/contracts/RussianWikiPipelineTest.java
+++ mathosphere-core/src/test/java/com/formulasearchengine/mathosphere/mlp/contracts/RussianWikiPipelineTest.java
@@ -3,37 +3,36 @@ package com.formulasearchengine.mathosphere.mlp.contracts;
 import com.formulasearchengine.mathosphere.mlp.cli.FlinkMlpCommandConfig;
 import com.formulasearchengine.mathosphere.mlp.pojos.ParsedWikiDocument;
 import com.formulasearchengine.mathosphere.mlp.pojos.RawWikiDocument;
-
 import org.junit.Test;
 
 import java.util.List;
 
 public class RussianWikiPipelineTest {
 
-  @Test
-  public void fullPipeline() throws Exception {
-    String wikiRuFile = "com/formulasearchengine/mathosphere/mlp/wikirusample.xml";
+    private static TextAnnotatorMapper ruTextAnnotator() throws Exception {
+        String[] params = {"--language", "ru", "-pos", "", "-in", "fake-input", "-out", "fake-output"};
+        FlinkMlpCommandConfig config = FlinkMlpCommandConfig.from(params);
 
-    TextAnnotatorMapper textAnnotator = ruTextAnnotator();
-
-    List<RawWikiDocument> docs = readDocs(wikiRuFile);
-
-    for (RawWikiDocument doc : docs) {
-      ParsedWikiDocument parsedDoc = textAnnotator.map(doc);
-      System.out.println(parsedDoc);
+        TextAnnotatorMapper textAnnotator = new TextAnnotatorMapper(config);
+        textAnnotator.open(null);
+        return textAnnotator;
     }
-  }
 
-  private List<RawWikiDocument> readDocs(String wikiRuFile) throws Exception {
-    return TextAnnotatorMapperTest.readWikiTextDocuments(wikiRuFile);
-  }
+    @Test
+    public void fullPipeline() throws Exception {
+        String wikiRuFile = "com/formulasearchengine/mathosphere/mlp/wikirusample.xml";
 
-  private static TextAnnotatorMapper ruTextAnnotator() throws Exception {
-    String[] params = {"--language", "ru", "-pos", ""};
-    FlinkMlpCommandConfig config = FlinkMlpCommandConfig.from(params);
+        TextAnnotatorMapper textAnnotator = ruTextAnnotator();
 
-    TextAnnotatorMapper textAnnotator = new TextAnnotatorMapper(config);
-    textAnnotator.open(null);
-    return textAnnotator;
-  }
+        List<RawWikiDocument> docs = readDocs(wikiRuFile);
+
+        for (RawWikiDocument doc : docs) {
+            ParsedWikiDocument parsedDoc = textAnnotator.map(doc);
+            System.out.println(parsedDoc);
+        }
+    }
+
+    private List<RawWikiDocument> readDocs(String wikiRuFile) throws Exception {
+        return TextAnnotatorMapperTest.readWikiTextDocuments(wikiRuFile);
+    }
 }
