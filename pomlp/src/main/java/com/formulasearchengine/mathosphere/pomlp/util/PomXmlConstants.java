58a1d26dd5a11afc0c45ca36cacd596c83dfbf44
AndreG-P <andre.greiner-petter@t-online.de>
2017-12-07 15:17:22 +0900

    Initialize POM-XML transformation

---
+++ pomlp/src/main/java/com/formulasearchengine/mathosphere/pomlp/util/PomXmlConstants.java
@@ -0,0 +1,17 @@
+package com.formulasearchengine.mathosphere.pomlp.util;
+
+public class PomXmlConstants {
+
+    public static final String NL = System.lineSeparator();
+
+    public static final String TAB = "  ";
+
+    public static final String EXPR_NODE = "expression";
+
+    public static final String TERM_NODE = "term";
+
+    public static final String ATTR_PRIME_TAG = "prime-tag";
+
+    public static final String ATTR_SEC_TAG = "secondary-tags";
+
+}
---
+++ pomlp/src/main/java/com/formulasearchengine/mathosphere/pomlp/xml/PomXmlWriter.java
@@ -0,0 +1,190 @@
+package com.formulasearchengine.mathosphere.pomlp.xml;
+
+import com.formulasearchengine.mathosphere.pomlp.util.PomlpConstants;
+import com.formulasearchengine.mathosphere.pomlp.util.PomlpInternalPaths;
+import com.sun.xml.internal.txw2.output.IndentingXMLStreamWriter;
+import gov.nist.drmf.interpreter.common.GlobalPaths;
+import mlp.MathTerm;
+import mlp.ParseException;
+import mlp.PomParser;
+import mlp.PomTaggedExpression;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.formulasearchengine.mathosphere.pomlp.util.PomXmlConstants.*;
+
+public class PomXmlWriter {
+
+    private static final Logger LOG = LogManager.getLogger( PomXmlWriter.class.getName() );
+
+    public static final String GENERIC_FILE_NAME = "pom-export.xml";
+
+    private static boolean overwrite = true;
+
+    private Path outputFile;
+
+    public PomXmlWriter() throws IOException {
+        outputFile = PomlpInternalPaths.ResourceXMLExportsDir.resolve( GENERIC_FILE_NAME );
+        initOverwriteCheck();
+    }
+
+    public PomXmlWriter( Path outputFilePath ) throws IOException {
+        outputFile = outputFilePath;
+        initOverwriteCheck();
+    }
+
+    private void initOverwriteCheck() throws IOException {
+        if ( Files.exists(outputFile) && !overwrite ){
+            String[] tmp = GENERIC_FILE_NAME.split("\\.");
+            String newFileName = tmp[0] + LocalDateTime.now().format(PomlpConstants.DATE_FORMAT);
+            outputFile = PomlpInternalPaths.ResourceXMLExportsDir.resolve(
+                    newFileName + "." + tmp[1]
+            );
+            LOG.info("Overwrite mode is off -> create different File: " + outputFile.getFileName());
+        } else if ( !Files.exists(outputFile) ){
+            Files.createFile( outputFile.toAbsolutePath() );
+            LOG.info("Create file: " + outputFile.toAbsolutePath());
+        } else {
+            LOG.info("Overwrite mode is on -> overwrite existing file " + outputFile.getFileName());
+        }
+    }
+
+    public void generateStraightXML( PomTaggedExpression root )
+            throws FileNotFoundException, XMLStreamException
+    {
+        LOG.debug("Start XML writing process...");
+        XMLOutputFactory factory = XMLOutputFactory.newInstance();
+        XMLStreamWriter writer = factory.createXMLStreamWriter(
+                new FileOutputStream( outputFile.toString() )
+        );
+
+        LOG.trace("Start document writing..");
+        // start
+        writer.writeStartDocument();
+        writer.writeCharacters( NL );
+        writer.writeStartElement("math");
+        writer.writeAttribute("type", "pom-exported");
+        writer.writeCharacters( NL );
+
+        LOG.trace("Start recursive writing..");
+        recursiveInnerXML( writer, root, 1 );
+        // write inner elements
+
+        LOG.trace("Close document.");
+        writer.writeEndElement();
+        writer.writeEndDocument();
+        writer.close();
+        LOG.info("XML writing successful!");
+    }
+
+    private void recursiveInnerXML( XMLStreamWriter writer, PomTaggedExpression root, int tabLevel )
+            throws XMLStreamException
+    {
+        writer.writeCharacters( generateTab(tabLevel) );
+        List<PomTaggedExpression> comps = root.getComponents();
+        MathTerm mathTerm = root.getRoot();
+        String pTag = root.getTag();
+        List<String> secTags = root.getSecondaryTags();
+        boolean termMode = false;
+
+        if ( mathTerm != null && !mathTerm.isEmpty() ){
+            pTag = mathTerm.getTag();
+            secTags = mathTerm.getSecondaryTags();
+            termMode = true;
+        }
+
+        if ( comps.isEmpty() ){ // recursive anchor
+            if ( termMode ) writer.writeStartElement( TERM_NODE );
+            else writer.writeEmptyElement( EXPR_NODE );
+
+            writer.writeAttribute( ATTR_PRIME_TAG, pTag );
+            if ( !secTags.isEmpty() )
+                writer.writeAttribute( ATTR_SEC_TAG, createListString(secTags) );
+
+            if ( termMode ){
+                writer.writeCharacters( mathTerm.getTermText() );
+                writer.writeEndElement();
+            }
+
+            writer.writeCharacters( NL );
+            return;
+        }
+
+        writer.writeStartElement( EXPR_NODE );
+        writer.writeAttribute( ATTR_PRIME_TAG, pTag );
+        if ( !secTags.isEmpty() )
+            writer.writeAttribute( ATTR_SEC_TAG, createListString(secTags) );
+        writer.writeCharacters( NL );
+
+        for ( PomTaggedExpression exp : comps ){
+            recursiveInnerXML( writer, exp, tabLevel+1 );
+        }
+
+        writer.writeCharacters( generateTab(tabLevel) );
+        writer.writeEndElement();
+        writer.writeCharacters( NL );
+    }
+
+    public static String createListString( List<String> list ){
+        if ( list.isEmpty() ) return "";
+        String out = "";
+        for ( String s : list )
+            out += s + ", ";
+        return out.substring(0, out.length()-2);
+    }
+
+    public static String generateTab( int level ){
+        String str = "";
+        for ( int i = 1; i <= level; i++ )
+            str += TAB;
+        return str;
+    }
+
+    public static void switchOverwriteModeOn(){
+        overwrite = true;
+    }
+
+    public static void switchOverwriteModeOff(){
+        overwrite = false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String eq = "P_n^{\\left(\\alpha,\\beta\\right)}\\left(\\cos\\left(a\\Theta\\right)\\right)";
+
+        PomXmlWriter writer = new PomXmlWriter(Paths.get("xml/pom-genericP.xml"));
+
+        //System.out.println( Paths.get("").toAbsolutePath() );
+        //System.out.println( Paths.get("").toAbsolutePath().getParent() );
+
+        Path refPath = Paths
+                .get("")
+                .toAbsolutePath()
+                .getParent()
+                .resolve( PomlpInternalPaths
+                        .LatexGrammarBaseDir
+                        .resolve(GlobalPaths.PATH_REFERENCE_DATA)
+                );
+
+        LOG.debug("Path to POM references: " + refPath.toAbsolutePath());
+
+        PomParser parser = new PomParser( refPath.toAbsolutePath().toString() );
+        try {
+            PomTaggedExpression pte = parser.parse(eq);
+            writer.generateStraightXML( pte );
+        } catch ( ParseException | FileNotFoundException | XMLStreamException e ){
+            LOG.error("Cannot parse a+b or cannot write XML!", e);
+        }
+    }
+}
