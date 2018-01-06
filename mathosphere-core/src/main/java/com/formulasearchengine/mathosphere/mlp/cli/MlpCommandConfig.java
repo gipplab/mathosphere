6409346c60ebadfe93fc8c499ba60e0302374703
AndreG-P <andre.greiner-petter@t-online.de>
2018-01-06 15:55:56 +0900

    Input is required for the MLP.

--- mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mlp/cli/MlpCommandConfig.java
+++ mathosphere-core/src/main/java/com/formulasearchengine/mathosphere/mlp/cli/MlpCommandConfig.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 @Parameters(commandDescription = "Applies the MLP algorithm to a single wiki article")
 public class MlpCommandConfig extends BaseConfig implements Serializable {
 
-  @Parameter(names = {"-in", "--inputFile"}, description = "path to the wiki article")
+  @Parameter(names = {"-in", "--inputFile"}, description = "path to the wiki article", required = true)
   private String input;
 
   @Parameter(names = {"-out", "--outputFile"}, description = "path to output file (if empty, print to stdout)")
